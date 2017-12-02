7637b603cdbfbe4799893023ed3f1f4cb6c3a1f1
Dimitri Wyzlic <dimitriwyzlic@gmail.com>
2017-12-02 12:53:55 +0100

    Add from github

--- app.py
+++ app.py
@@ -23,34 +23,26 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    log("JE SUIS ICI MDR");
     # endpoint for processing incoming messaging events
-
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
     if data["object"] == "page":
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-
                 if messaging_event.get("message"):  # someone sent us a message
-
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "roger that!")
+                    send_message(sender_id, "tu m'a envoye: " + message_text)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
+                    # send_message(sender_id, "t'a recu mon message... trop cool !")
 
                 if messaging_event.get("optin"):  # optin confirmation
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
-
     return "ok", 200
 
 
@@ -74,20 +66,13 @@ def send_message(recipient_id, message_text):
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
+        log("Status code != 200")
         log(r.status_code)
         log(r.text)
 
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        else:
-            msg = unicode(msg).format(*args, **kwargs)
-        print u"{}: {}".format(datetime.now(), msg)
-    except UnicodeEncodeError:
-        print "Unicode Encode Error"
-        pass  # squash logging errors in case of non-ascii text
+    print msg
     sys.stdout.flush()
 
 
