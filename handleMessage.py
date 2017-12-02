68aec93c9609c69e8723ea6149ba5d8821af21e3
Dimitri Wyzlic <dimitriwyzlic@gmail.com>
2017-12-02 18:58:08 +0100

    Fix error

--- handleMessage.py
+++ handleMessage.py
@@ -32,30 +32,10 @@ def handle(user, msg, page):
     if city is None and theme is None:
         page.send(user, out)
         return
-
-    page.send(user, Template.Generic([
-            Template.GenericElement("rift",
-                                    subtitle="Next-generation virtual reality",
-                                    item_url="https://www.oculus.com/en-us/rift/",
-                                    image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png",
-                                    buttons=[
-                                        Template.ButtonWeb("Open Web URL", "https://www.oculus.com/en-us/rift/"),
-                                        Template.ButtonPostBack("tigger Postback", "DEVELOPED_DEFINED_PAYLOAD"),
-                                        Template.ButtonPhoneNumber("Call Phone Number", "+16505551234")
-                                    ]),
-            Template.GenericElement("touch",
-                                    subtitle="Your Hands, Now in VR",
-                                    item_url="https://www.oculus.com/en-us/touch/",
-                                    image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png",
-                                    buttons=[
-                                        Template.ButtonWeb("Open Web URL", "https://www.oculus.com/en-us/rift/"),
-                                        Template.ButtonPostBack("tigger Postback", "DEVELOPED_DEFINED_PAYLOAD"),
-                                        Template.ButtonPhoneNumber("Call Phone Number", "+16505551234")
-                                    ])
-        ]))
-    # else:
-    #     getEv = ev.getEvent(city, '5', theme)
-    #     print(getEv)
-    #     for tmp in getEv:
-    #         tList.append(ev.formatEvent(tmp))
-    # page.send(user, Template.Generic(tList))
+    else:
+        getEv = ev.getEvent(city, '5', theme)
+        print(getEv)
+        for tmp in getEv:
+            eventName, eventDate, eventLink, eventAdress, eventCity = ev.formatEvent(tmp)
+            tList.append(addTemplate(eventName, eventDate, eventLink, eventAdress, eventCity))
+        page.send(user, Template.Generic(tList))
